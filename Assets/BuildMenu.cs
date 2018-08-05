38729749b0a6af212fac299de26e76d05fd65e3d
macattackftw <macattackftw@gmail.com>
2018-08-04 22:00:21 -0600

    Added sound objects
    Added background sound and OnPointerEnter functionality to the menu buttons. Removed MonoBehaviour from MenuSounds.

--- Assets/BuildMenu.cs
+++ Assets/BuildMenu.cs
@@ -1,46 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class BuildMenu : MonoBehaviour {
-
+    
     public GameObject menuButton;
     public GameObject panel;
     public GameObject canvas;
 
-    private static string sceneName = "MainMenu";
-    private List<GameObject> buttons = new List<GameObject>();    
+    //private static MenuState menuState = MenuState.NONE;
+    private List<GameObject> buttons = new List<GameObject>();
     private GameObject canvasMain;
     private GameObject panelMain;
+    private MenuSounds menuSounds;
+    private EventTrigger eventTrigger = null;
 
     private const float btnBuffer = 0.60f;
     private const int mainMenuButtonCount = 4;
     private const int optionsMenuButtonCount = 4;
 
     // Main Menu
-    private const string NewGame = "New Game";
-    private const string LoadGame = "Load";
-    private const string OptionsMenu = "Options";
-    private const string QuitGame = "Quit";
+    private const string strMainMenu = "MainMenu";
+    private const string strNewGame = "New Game";
+    private const string strLoadGame = "Load";
+    private const string strOptionsMenu = "Options";
+    private const string strQuitGame = "Quit";
 
     // Options Menu
-    private const string MasterVolume = "Master Volume";
-    private const string SFXVolume = "Sound Effects Volume";
-    private const string MusicVolume = "Music Volume";
+    private const string strMasterVolume = "Master Volume";
+    private const string strSFXVolume = "Sound Effects Volume";
+    private const string strMusicVolume = "Music Volume";
 
     // Common Menu
-    private const string Back = "Back";
+    private const string strBack = "Back";
 
-    public enum MenuState
-    {
-        MAINMENU,
-        PAUSEMENU,
-        OPTIONSMENU,
-        LOAD,
-        SAVE,
-    }
+    public static MenuState MenuState { get; set; } = MenuState.NONE;
 
     public enum MenuType
     {
@@ -52,10 +48,8 @@ public class BuildMenu : MonoBehaviour {
 
     private void Awake()
     {
-        if (SceneManager.GetActiveScene().name == "MainMenu")
-        {
-            LoadMenu(MenuState.MAINMENU);
-        }
+        menuSounds = new MenuSounds();
+        LoadMenu();        
     }
 
     // Use this for initialization
@@ -69,12 +63,16 @@ public class BuildMenu : MonoBehaviour {
 	}
 
 
-    private void LoadMenu(MenuState menuState)
+    
+    private void LoadMenu()
     {
         buttons.Clear();
 
-        switch (menuState)
+        switch (MenuState)
         {
+            case MenuState.NONE:
+                MainMenuLoad();
+                break;
             case MenuState.MAINMENU:
                 MainMenuLoad();
                 break;
@@ -104,7 +102,9 @@ public class BuildMenu : MonoBehaviour {
     /// Loads the Main Menu and associated buttons.
     /// </summary>
     public void MainMenuLoad()
-    {
+    {        
+        menuSounds.StartMenuSounds();
+
         LoadCanvasPanel();
         RectTransform panelRect = panelMain.GetComponent<RectTransform>();
         RectTransform btnRect = menuButton.GetComponent<RectTransform>();
@@ -125,10 +125,11 @@ public class BuildMenu : MonoBehaviour {
         btnPosition.y += menuButton.GetComponent<RectTransform>().rect.height;
 
         buttons.Clear();
-        buttons.Add(InstantiateAdjustObj(menuButton, NewGame, ref btnPosition, MenuType.MAIN, panelMain));
-        buttons.Add(InstantiateAdjustObj(menuButton, LoadGame, ref btnPosition, MenuType.MAIN, panelMain));
-        buttons.Add(InstantiateAdjustObj(menuButton, OptionsMenu, ref btnPosition, MenuType.MAIN, panelMain));
-        buttons.Add(InstantiateAdjustObj(menuButton, QuitGame, ref btnPosition, MenuType.MAIN, panelMain));
+        buttons.Add(InstantiateAdjustObj(menuButton, strNewGame, ref btnPosition, MenuType.MAIN, panelMain));
+        buttons.Add(InstantiateAdjustObj(menuButton, strLoadGame, ref btnPosition, MenuType.MAIN, panelMain));
+        buttons.Add(InstantiateAdjustObj(menuButton, strOptionsMenu, ref btnPosition, MenuType.MAIN, panelMain));
+        buttons.Add(InstantiateAdjustObj(menuButton, strQuitGame, ref btnPosition, MenuType.MAIN, panelMain));
+        MenuState = MenuState.MAINMENU;
     }
 
 
@@ -138,19 +139,20 @@ public class BuildMenu : MonoBehaviour {
     /// <param name="obj">The object that requires an interaction</param>
     private void HandleMainMenuClicks(GameObject obj)
     {
+        //menuSounds.HoverSoundPlay();
         switch (obj.name)
         {
-            case "New Game":
+            case strNewGame:
                 Debug.Log("GOT New Game!");
                 break;
-            case "Load":
+            case strLoadGame:
                 Debug.Log("GOT Load!");
                 break;
-            case "Options":
+            case strOptionsMenu:
                 Debug.Log("GOT Options!");
                 OptionsMenuLoad();
                 break;
-            case "Quit":
+            case strQuitGame:
                 Debug.Log("GOT Quit!");
 #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
@@ -172,17 +174,17 @@ public class BuildMenu : MonoBehaviour {
     {
         switch (obj.name)
         {
-            case "Master Volume":
+            case strMasterVolume:
                 Debug.Log("GOT Master Volume!");
                 break;
-            case "SFX Volume":
+            case strSFXVolume:
                 Debug.Log("GOT SFX Volume!");
                 break;
-            case "Music Volume":
+            case strMusicVolume:
                 OptionsMenuLoad();
                 Debug.Log("GOT Music Volume!");
                 break;
-            case "Back":
+            case strBack:
                 Debug.Log("GOT Back!");
                 MainMenuLoad();
                 break;
@@ -216,10 +218,11 @@ public class BuildMenu : MonoBehaviour {
         // Offset function y movement
         btnPosition.y += menuButton.GetComponent<RectTransform>().rect.height;
 
-        InstantiateAdjustObj(menuButton, MasterVolume, ref btnPosition, MenuType.OPTIONS, panelMain);
-        InstantiateAdjustObj(menuButton, SFXVolume, ref btnPosition, MenuType.OPTIONS, panelMain);
-        InstantiateAdjustObj(menuButton, MusicVolume, ref btnPosition, MenuType.OPTIONS, panelMain);
-        InstantiateAdjustObj(menuButton, Back, ref btnPosition, MenuType.OPTIONS, panelMain);
+        InstantiateAdjustObj(menuButton, strMasterVolume, ref btnPosition, MenuType.OPTIONS, panelMain);
+        InstantiateAdjustObj(menuButton, strSFXVolume, ref btnPosition, MenuType.OPTIONS, panelMain);
+        InstantiateAdjustObj(menuButton, strMusicVolume, ref btnPosition, MenuType.OPTIONS, panelMain);
+        InstantiateAdjustObj(menuButton, strBack, ref btnPosition, MenuType.OPTIONS, panelMain);
+        MenuState = MenuState.OPTIONSMENU;
     }
 
 
@@ -231,24 +234,27 @@ public class BuildMenu : MonoBehaviour {
     /// <param name="vec">Reference to offset vector</param>
     /// <param name="Parent">The parent of the created GameObject</param>
     /// <returns>The created GameObject</returns>
-    private GameObject InstantiateAdjustObj(GameObject Obj, string DisplayText, ref Vector3 vec, MenuType menuType, GameObject Parent = null)
+    private GameObject InstantiateAdjustObj(GameObject Obj, string DisplayText, ref Vector3 Vec, MenuType MenuType, GameObject Parent = null)
     {
         GameObject obj = Instantiate(Obj) as GameObject;
         obj.GetComponentInChildren<Text>().text = DisplayText;
         obj.name = DisplayText;
         obj.transform.SetParent(Parent.transform);
-        vec.y -= obj.GetComponent<RectTransform>().rect.height;
-        obj.transform.position = vec;
+        Vec.y -= obj.GetComponent<RectTransform>().rect.height;
+        obj.transform.position = Vec;
 
         Button btn = obj.GetComponent<Button>();
+        eventTrigger = obj.AddComponent<EventTrigger>();
+        AddEventTrigger(OnPointerEnter, EventTriggerType.PointerEnter);
+
         if (btn != null)
         {
             // TODO: These should be enums?
-            if (menuType == MenuType.MAIN)
+            if (MenuType == MenuType.MAIN)
             {
                 btn.onClick.AddListener(delegate { HandleMainMenuClicks(obj); });
             }
-            else if (menuType == MenuType.OPTIONS)
+            else if (MenuType == MenuType.OPTIONS)
             {
                 btn.onClick.AddListener(delegate { HandleOptionsMenuClicks(obj); });                
             }
@@ -256,6 +262,37 @@ public class BuildMenu : MonoBehaviour {
 
         return obj;
     }
+
+
+    // https://answers.unity.com/questions/781726/how-do-i-add-a-listener-to-onpointerenter-ugui.html
+    private void AddEventTrigger(UnityAction action, EventTriggerType triggerType)
+    {
+        // Create a new TriggerEvent and add a listener
+        EventTrigger.TriggerEvent trigger = new EventTrigger.TriggerEvent();
+        trigger.AddListener((eventData) => action()); // you can capture and pass the event data to the listener
+
+        // Create and initialise EventTrigger.Entry using the created TriggerEvent
+        EventTrigger.Entry entry = new EventTrigger.Entry() { callback = trigger, eventID = triggerType };
+
+        // Add the EventTrigger.Entry to delegates list on the EventTrigger
+        eventTrigger.triggers.Add(entry);
+    }
     
 
+    private void OnPointerEnter()
+    {
+        menuSounds.HoverSoundPlay();
+    }
+    
+
+}
+
+public enum MenuState
+{
+    NONE,
+    MAINMENU,
+    PAUSEMENU,
+    OPTIONSMENU,
+    LOAD,
+    SAVE,
 }
---
+++ Assets/Plugins/MenuSounds.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class MenuSounds
+{
+
+    private AudioSource backgroundMusic;
+    private AudioSource hoverSound;
+    private GameObject gameObject = null;
+    private AudioMixer musicMixer;
+
+    private const string strMusicMixer = "Music";
+    private const string strSFXMixer = "SFX";
+    private const string strMusicPath = "Audio/Spybreak!";
+    private const string strHoverSoundPath = "Audio/computerbeep";
+
+
+    public void StartMenuSounds()
+    {
+        if (gameObject == null || !backgroundMusic.isPlaying)
+        {
+            musicMixer = Resources.Load<AudioMixer>("Audio/MainMixer") as AudioMixer;
+            StartBackgroundMusic();
+            PrepareHoverSounds();
+        }
+    }
+
+    private void StartBackgroundMusic()
+    {
+        gameObject = new GameObject { name = "Audio Object" };
+        backgroundMusic = gameObject.AddComponent<AudioSource>();
+        backgroundMusic.clip = Resources.Load<AudioClip>(strMusicPath);
+
+        backgroundMusic.outputAudioMixerGroup = musicMixer.FindMatchingGroups(strMusicMixer)[0];
+        backgroundMusic.playOnAwake = false;
+        backgroundMusic.loop = true;
+        backgroundMusic.Play();
+
+    }
+
+    private void PrepareHoverSounds()
+    {
+        hoverSound = gameObject.AddComponent<AudioSource>();
+        hoverSound.clip = Resources.Load<AudioClip>(strHoverSoundPath);
+        hoverSound.outputAudioMixerGroup = musicMixer.FindMatchingGroups(strSFXMixer)[0];
+        hoverSound.playOnAwake = false;
+    }
+
+    public void HoverSoundPlay()
+    {
+        hoverSound.Stop();
+        hoverSound.Play();
+    }
+
+    public void StopMusic()
+    {
+        backgroundMusic.Stop();
+        backgroundMusic = null;
+
+        gameObject = null;
+
+    }
+}
